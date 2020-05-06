ef9f7a11095d24a837c335aaad3fbe0d8863c29a
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 00:11:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(argument)
   i = 0
   first_name_array = []
   while i < argument.length
-    collection  << yield argument[i]
+    collection << yield argument[i]
     i += 1
   end
   collection
